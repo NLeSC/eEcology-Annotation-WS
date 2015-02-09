4db9617b11375bd0a0b43196a26a05a0f11505c8
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-02-09 10:26:24 +0100

    Remove unused code.

--- annotation/static/table.js
+++ annotation/static/table.js
@@ -2,5 +2,4 @@ function loadTrackerAndAnnotation(classes, annotationsUrl) {
 	var ctrl = TrackAnnot.getApplication().getController('Main');
 	ctrl.getClassificationsStore().loadRawData(classes);
     ctrl.getAnnotationsStore().enableRemoteMode(annotationsUrl);
-    var button = Ext.ComponentQuery.query('button[action=loadTrack]')[0];
 }
